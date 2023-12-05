70741a822d1ab420e78c925bb51ec6754b3c998c
Anton Obukhov <4390695+toshas@users.noreply.github.com>
2023-12-05 02:39:04 +0100

    Update download_weights.sh
    add a -p flag to mkdir to never fail
    check if the extracted checkpoint exists before downloading it again

--- script/download_weights.sh
+++ script/download_weights.sh
@@ -3,8 +3,13 @@ set -e
 set -x
 
 ckpt_dir=${ckpt_dir:-checkpoint}
-mkdir $ckpt_dir
+mkdir -p $ckpt_dir
 cd $ckpt_dir
+
+if [ -d Marigold_v1_merged ]; then
+    exit 0
+fi
+
 wget -nv --show-progress https://share.phys.ethz.ch/~pf/bingkedata/marigold/Marigold_v1_merged.tar
 
 tar -xf Marigold_v1_merged.tar
