585b19061759e1b997efee86fbf473636d67f48c
Anton Obukhov <4390695+toshas@users.noreply.github.com>
2023-12-05 02:19:49 +0100

    Update download_weights.sh
    allow specifying external location for the checkpoint weights

--- script/download_weights.sh
+++ script/download_weights.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-ckpt_dir=checkpoint
+ckpt_dir=${ckpt_dir:-checkpoint}
 mkdir $ckpt_dir
 cd $ckpt_dir
 wget -nv --show-progress https://share.phys.ethz.ch/~pf/bingkedata/marigold/Marigold_v1_merged.tar
