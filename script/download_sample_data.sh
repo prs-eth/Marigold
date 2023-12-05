1238f64f93d5ca2ea76afddd8ef9b3ea6f3c68bf
Bingxin Ke <bingke@ethz.ch>
2023-12-05 09:38:13 +0100

    [UPDATE] download_sample_data script add a -p flag to mkdir to never fail stop script if tar file exists

--- script/download_sample_data.sh
+++ script/download_sample_data.sh
@@ -3,8 +3,14 @@ set -e
 set -x
 
 data_dir=data
-mkdir $data_dir
+mkdir -p $data_dir
 cd $data_dir
+
+if test -f "in-the-wild_example.tar" ; then
+    echo "Tar file exists: in-the-wild_example.tar"
+    exit 1
+fi
+
 wget -nv --show-progress https://share.phys.ethz.ch/~pf/bingkedata/marigold/in-the-wild_example.tar
 
 tar -xf in-the-wild_example.tar
