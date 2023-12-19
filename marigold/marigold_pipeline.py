[ADD] half-precision (#20); [FIX] scale #21; enable fp16 inference; clean cache after unet; test on multiple GPUs; fix single_infer scaling ;

marigold/marigold_pipeline.py
@@ -1,5 +1,5 @@
 # Author: Bingxin Ke
-# Last modified: 2023-12-15
+# Last modified: 2023-12-19
 
 from typing import List, Dict, Union
 
@@ -157,7 +157,7 @@ class MarigoldPipeline(DiffusionPipeline):
         # Normalize rgb values
         rgb = np.transpose(image, (2, 0, 1))  # [H, W, rgb] -> [rgb, H, W]
         rgb_norm = rgb / 255.0
-        rgb_norm = torch.from_numpy(rgb_norm).float()
+        rgb_norm = torch.from_numpy(rgb_norm).to(self.dtype)
         rgb_norm = rgb_norm.to(device)
         assert rgb_norm.min() >= 0.0 and rgb_norm.max() <= 1.0
 
@@ -169,7 +169,9 @@ class MarigoldPipeline(DiffusionPipeline):
             _bs = batch_size
         else:
             _bs = find_batch_size(
-                ensemble_size=ensemble_size, input_res=max(rgb_norm.shape[1:])
+                ensemble_size=ensemble_size,
+                input_res=max(rgb_norm.shape[1:]),
+                dtype=self.dtype,
             )
 
         single_rgb_loader = DataLoader(
@@ -211,7 +213,7 @@ class MarigoldPipeline(DiffusionPipeline):
         depth_pred = (depth_pred - min_d) / (max_d - min_d)
 
         # Convert to numpy
-        depth_pred = depth_pred.cpu().numpy()
+        depth_pred = depth_pred.cpu().numpy().astype(np.float32)
 
         # Resize back to original resolution
         if match_input_res:
@@ -248,7 +250,7 @@ class MarigoldPipeline(DiffusionPipeline):
             return_tensors="pt",
         )
         text_input_ids = text_inputs.input_ids.to(self.text_encoder.device)
-        self.empty_text_embed = self.text_encoder(text_input_ids)[0]
+        self.empty_text_embed = self.text_encoder(text_input_ids)[0].to(self.dtype)
 
     @torch.no_grad()
     def single_infer(
@@ -278,7 +280,9 @@ class MarigoldPipeline(DiffusionPipeline):
         rgb_latent = self.encode_rgb(rgb_in)
 
         # Initial depth map (noise)
-        depth_latent = torch.randn(rgb_latent.shape, device=device)  # [B, 4, h, w]
+        depth_latent = torch.randn(
+            rgb_latent.shape, device=device, dtype=self.dtype
+        )  # [B, 4, h, w]
 
         # Batched empty text embedding
         if self.empty_text_embed is None:
@@ -310,12 +314,13 @@ class MarigoldPipeline(DiffusionPipeline):
 
             # compute the previous noisy sample x_t -> x_t-1
             depth_latent = self.scheduler.step(noise_pred, t, depth_latent).prev_sample
+        torch.cuda.empty_cache()  # clear vram cache for ensembling
         depth = self.decode_depth(depth_latent)
 
         # clip prediction
         depth = torch.clip(depth, -1.0, 1.0)
         # shift to [0, 1]
-        depth = depth * 2.0 - 1.0
+        depth = (depth + 1.0) / 2.0
 
         return depth
 

marigold/util/batchsize.py
@@ -1,5 +1,5 @@
 # Author: Bingxin Ke
-# Last modified: 2023-12-15
+# Last modified: 2023-12-19
 
 import torch
 import math
@@ -8,22 +8,30 @@ import math
 # Search table for suggested max. inference batch size
 bs_search_table = [
     # tested on A100-PCIE-80GB
-    {"res": 768, "total_vram": 79, "bs": 35},
-    {"res": 1024, "total_vram": 79, "bs": 20},
+    {"res": 768, "total_vram": 79, "bs": 35, "dtype": torch.float32},
+    {"res": 1024, "total_vram": 79, "bs": 20, "dtype": torch.float32},
     # tested on A100-PCIE-40GB
-    {"res": 768, "total_vram": 39, "bs": 15},
-    {"res": 1024, "total_vram": 39, "bs": 8},
+    {"res": 768, "total_vram": 39, "bs": 15, "dtype": torch.float32},
+    {"res": 1024, "total_vram": 39, "bs": 8, "dtype": torch.float32},
+    {"res": 768, "total_vram": 39, "bs": 30, "dtype": torch.float16},
+    {"res": 1024, "total_vram": 39, "bs": 15, "dtype": torch.float16},
     # tested on RTX3090, RTX4090
-    {"res": 512, "total_vram": 23, "bs": 20},
-    {"res": 768, "total_vram": 23, "bs": 7},
-    {"res": 1024, "total_vram": 23, "bs": 3},
+    {"res": 512, "total_vram": 23, "bs": 20, "dtype": torch.float32},
+    {"res": 768, "total_vram": 23, "bs": 7, "dtype": torch.float32},
+    {"res": 1024, "total_vram": 23, "bs": 3, "dtype": torch.float32},
+    {"res": 512, "total_vram": 23, "bs": 40, "dtype": torch.float16},
+    {"res": 768, "total_vram": 23, "bs": 18, "dtype": torch.float16},
+    {"res": 1024, "total_vram": 23, "bs": 10, "dtype": torch.float16},
     # tested on GTX1080Ti
-    {"res": 512, "total_vram": 10, "bs": 5},
-    {"res": 768, "total_vram": 10, "bs": 2},
+    {"res": 512, "total_vram": 10, "bs": 5, "dtype": torch.float32},
+    {"res": 768, "total_vram": 10, "bs": 2, "dtype": torch.float32},
+    {"res": 512, "total_vram": 10, "bs": 10, "dtype": torch.float16},
+    {"res": 768, "total_vram": 10, "bs": 5, "dtype": torch.float16},
+    {"res": 1024, "total_vram": 10, "bs": 3, "dtype": torch.float16},
 ]
 
 
-def find_batch_size(ensemble_size: int, input_res: int) -> int:
+def find_batch_size(ensemble_size: int, input_res: int, dtype: torch.dtype) -> int:
     """
     Automatically search for suitable operating batch size.
 
@@ -38,8 +46,11 @@ def find_batch_size(ensemble_size: int, input_res: int) -> int:
         return 1
 
     total_vram = torch.cuda.mem_get_info()[1] / 1024.0**3
-
-    for settings in sorted(bs_search_table, key=lambda k: (k["res"], -k["total_vram"])):
+    filtered_bs_search_table = [s for s in bs_search_table if s["dtype"] == dtype]
+    for settings in sorted(
+        filtered_bs_search_table,
+        key=lambda k: (k["res"], -k["total_vram"]),
+    ):
         if input_res <= settings["res"] and total_vram >= settings["total_vram"]:
             bs = settings["bs"]
             if bs > ensemble_size:

marigold/util/ensemble.py
@@ -1,6 +1,6 @@
 # Test align depth images
 # Author: Bingxin Ke
-# Last modified: 2023-12-15
+# Last modified: 2023-12-18
 
 import numpy as np
 import torch
@@ -34,7 +34,8 @@ def ensemble_depths(
         by aligning estimating the scale and shift
     """
     device = input_images.device
-    dtype = np.float32
+    dtype = input_images.dtype
+    np_dtype = np.float32
 
     original_input = input_images.clone()
     n_img = input_images.shape[0]
@@ -51,18 +52,17 @@ def ensemble_depths(
     _max = np.max(input_images.reshape((n_img, -1)).cpu().numpy(), axis=1)
     s_init = 1.0 / (_max - _min).reshape((-1, 1, 1))
     t_init = (-1 * s_init.flatten() * _min.flatten()).reshape((-1, 1, 1))
-    x = np.concatenate([s_init, t_init]).reshape(-1)
+    x = np.concatenate([s_init, t_init]).reshape(-1).astype(np_dtype)
 
     input_images = input_images.to(device)
 
     # objective function
     def closure(x):
-        x = x.astype(dtype)
         l = len(x)
         s = x[: int(l / 2)]
         t = x[int(l / 2) :]
-        s = torch.from_numpy(s).to(device)
-        t = torch.from_numpy(t).to(device)
+        s = torch.from_numpy(s).to(dtype=dtype).to(device)
+        t = torch.from_numpy(t).to(dtype=dtype).to(device)
 
         transformed_arrays = input_images * s.view((-1, 1, 1)) + t.view((-1, 1, 1))
         dists = inter_distances(transformed_arrays)
@@ -79,21 +79,20 @@ def ensemble_depths(
         far_err = torch.sqrt((1 - torch.max(pred)) ** 2)
 
         err = sqrt_dist + (near_err + far_err) * regularizer_strength
-        err = err.detach().cpu().numpy()
+        err = err.detach().cpu().numpy().astype(np_dtype)
         return err
 
     res = minimize(
         closure, x, method="BFGS", tol=tol, options={"maxiter": max_iter, "disp": False}
     )
     x = res.x
-    x = x.astype(dtype)
     l = len(x)
     s = x[: int(l / 2)]
     t = x[int(l / 2) :]
 
     # Prediction
-    s = torch.from_numpy(s).to(device)
-    t = torch.from_numpy(t).to(device)
+    s = torch.from_numpy(s).to(dtype=dtype).to(device)
+    t = torch.from_numpy(t).to(dtype=dtype).to(device)
     transformed_arrays = original_input * s.view(-1, 1, 1) + t.view(-1, 1, 1)
     if "mean" == reduction:
         aligned_images = torch.mean(transformed_arrays, dim=0)

run.py
@@ -1,7 +1,7 @@
 # Script for inference on (in-the-wild) images
 
 # Author: Bingxin Ke
-# Last modified: 2023-12-15
+# Last modified: 2023-12-18
 
 
 import argparse
@@ -60,6 +60,11 @@ if "__main__" == __name__:
         default=10,
         help="Number of predictions to be ensembled, more inference gives better results but runs slower.",
     )
+    parser.add_argument(
+        "--half_precision",
+        action="store_true",
+        help="Run with half-precision (16-bit float), might lead to suboptimal result.",
+    )
 
     # resolution setting
     parser.add_argument(
@@ -104,6 +109,9 @@ if "__main__" == __name__:
 
     denoise_steps = args.denoise_steps
     ensemble_size = args.ensemble_size
+    if ensemble_size > 15:
+        logging.warning(f"Running with large ensemble size will be slow.")
+    half_precision = args.half_precision
 
     processing_res = args.processing_res
     match_input_res = not args.output_processing_res
@@ -131,9 +139,9 @@ if "__main__" == __name__:
     os.makedirs(output_dir_color, exist_ok=True)
     os.makedirs(output_dir_tif, exist_ok=True)
     os.makedirs(output_dir_npy, exist_ok=True)
-    logging.info(f"output dir: {output_dir}")
+    logging.info(f"output dir = {output_dir}")
 
-    # Device
+    # -------------------- Device --------------------
     if apple_silicon:
         if torch.backends.mps.is_available() and torch.backends.mps.is_built():
             device = torch.device("mps:0")
@@ -146,7 +154,7 @@ if "__main__" == __name__:
         else:
             device = torch.device("cpu")
             logging.warning("CUDA is not available. Running on CPU will be slow.")
-    logging.info(f"device: {device}")
+    logging.info(f"device = {device}")
 
     # -------------------- Data --------------------
     rgb_filename_list = glob(os.path.join(input_rgb_dir, "*"))
@@ -162,7 +170,14 @@ if "__main__" == __name__:
         exit(1)
 
     # -------------------- Model --------------------
-    pipe = MarigoldPipeline.from_pretrained(checkpoint_path)
+    if half_precision:
+        dtype = torch.float16
+        logging.info(f"Running with half precision ({dtype}).")
+    else:
+        dtype = torch.float32
+
+    pipe = MarigoldPipeline.from_pretrained(checkpoint_path, torch_dtype=dtype)
+    
     try:
         import xformers
         pipe.enable_xformers_memory_efficient_attention()
@@ -214,5 +229,7 @@ if "__main__" == __name__:
                 output_dir_color, f"{pred_name_base}_colored.png"
             )
             if os.path.exists(colored_save_path):
-                logging.warning(f"Existing file: '{colored_save_path}' will be overwritten")
+                logging.warning(
+                    f"Existing file: '{colored_save_path}' will be overwritten"
+                )
             depth_colored.save(colored_save_path)